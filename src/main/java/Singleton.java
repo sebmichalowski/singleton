b5c70e18e5aad2bde8117598a21e163e03e310b2
sebmichalowski <seb.michalowski@gmail.com>
2017-06-27 03:45:03 +0200

    Implement Singleton

---
+++ src/main/java/Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton {
+    private static Singleton singleton = null;
+
+    private Singleton(){}
+
+    public static Singleton getSingleton() {
+        if (singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+}
